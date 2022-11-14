ef77a16d117ca2cccd87c85ce22d4788b9821f4a
Tobias <tobschr@web.de>
2022-11-14 12:21:03 +0100

    improved message

--- src/main/java/de/Skippero/LOA/events/OnSlashCommandInteraction.java
+++ src/main/java/de/Skippero/LOA/events/OnSlashCommandInteraction.java
@@ -210,8 +210,8 @@ public class OnSlashCommandInteraction extends ListenerAdapter {
             }
 
             if(!userSelection.contains(-1) && !userSelection.contains(-2)) {
-                event.reply("You did not register your Server yet, please provide this command with the correct Id ⮕ '/vendor server 0/1'").setEphemeral(true).queue();
-                event.reply("0 ⮕ Ealyn\n" + "1 ⮕ Nia").setEphemeral(true).queue();
+                event.reply("You did not register your Server yet, please provide this command with the correct Id ⮕ '/vendor server 0/1'\n"+"0 ⮕ Ealyn\n" + "1 ⮕ Nia").setEphemeral(true).queue();
+                event.reply("").setEphemeral(true).queue();
             }
 
             if (event.getOptions().size() == 1) {
