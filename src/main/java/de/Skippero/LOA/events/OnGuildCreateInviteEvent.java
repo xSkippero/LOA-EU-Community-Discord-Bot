067c77e9112788a475b8410a22f554d32e01ea30
Tobias <tobschr@web.de>
2024-03-18 16:01:01 +0100

    Delete Button for Invite Deletion Message

--- src/main/java/de/Skippero/LOA/events/OnGuildCreateInviteEvent.java
+++ src/main/java/de/Skippero/LOA/events/OnGuildCreateInviteEvent.java
@@ -12,10 +12,13 @@ on 18.03.2024 , 15:43
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.invite.GuildInviteCreateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import java.util.List;
 
 public class OnGuildCreateInviteEvent extends ListenerAdapter {
+
+    Button delButton = Button.danger("del","Delete");
 
     public void OnGuildCreateInvite(GuildInviteCreateEvent event) {
         if(event.getInvite().getMaxAge() != 0) {
@@ -25,7 +28,8 @@ public class OnGuildCreateInviteEvent extends ListenerAdapter {
         long userId = event.getInvite().getInviter().getIdLong();
         List<Role> userRoles = event.getGuild().getMemberById(userId).getRoles();
         if(userRoles.stream().noneMatch(r->r.getName().equalsIgnoreCase("Permanent Invites"))) {
-            event.getInvite().getInviter().openPrivateChannel().flatMap(c->c.sendMessage("Sorry, but you do not have the required permissions to create permanent invite links, for a permanent link please use the one in the pinned message in #\uD83D\uDD14announcements")).queue();
+            event.getInvite().getInviter().openPrivateChannel().flatMap(channel -> channel.sendMessage("[Automated Message] Sorry, but you do not have the required permissions to create permanent invite links," +
+                    " for a permanent link please use the one in the pinned message in #\uD83D\uDD14announcements :smile:").setActionRow(delButton)).queue();
             event.getInvite().delete().queue();
         }
 
